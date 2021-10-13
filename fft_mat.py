97e6b923740edf57975c7103767e45c059793692
Barrack Ha <barakiey@gmail.com>
2021-10-13 16:37:34 +0300

    fft bugfix

--- fft_mat.py
+++ fft_mat.py
@@ -2,26 +2,28 @@ import numpy as np
 import pandas as pd
 import scipy.io as sio
 import matplotlib.pyplot as plt
-from scipy.fft import fft
+from scipy.fft import fft, fftfreq
 
-mat = sio.loadmat('data_files/AIM_model_movements0004.mat')
+# mat = sio.loadmat('data_files/AIM_model_movements0004.mat')
+file_name = 'Barak_test'
+mat = sio.loadmat('data_files/{}'.format(file_name))
 
 # print(mat['AIM_model_movements0004'].dtype)
 
 # print(mat['AIM_model_movements0004']['File'])
-file_timestamp = mat['AIM_model_movements0004']['Timestamp'][0][0].flatten()
+file_timestamp = mat[file_name]['Timestamp'][0][0].flatten()
 # print(file_timestamp)
 # print(mat['AIM_model_movements0004']['StartFrame'])
-num_of_frames = mat['AIM_model_movements0004']['Frames'][0][0].flatten()[0]
+num_of_frames = mat[file_name]['Frames'][0][0].flatten()[0]
 delta_t_sec = 1 / num_of_frames
 # print(type(delta_t_sec))
-frame_rate = mat['AIM_model_movements0004']['FrameRate'][0][0].flatten()[0]
+frame_rate = mat[file_name]['FrameRate'][0][0].flatten()[0]
 # print(frame_rate)
 # print(mat['AIM_model_movements0004']['Events'])
 # print(mat['AIM_model_movements0004']['Trajectories'][0][0]['Labeled'][0][0].dtype)
 # print(mat['AIM_model_movements0004']['Trajectories'][0][0]['Unidentified'])
 
-data = mat['AIM_model_movements0004']['Trajectories'][0][0]['Labeled'][0][0]
+data = mat[file_name]['Trajectories'][0][0]['Labeled'][0][0]
 # 'Count', 'O'), ('Labels', 'O'), ('Data', 'O'), ('Type', 'O'), ('TrajectoryType', 'O')]
 lables = data['Labels'][0][0][0].flatten()
 # print(len(lables))
@@ -38,7 +40,7 @@ data = data['Data'][0][0]
 data_df_list = [ pd.DataFrame(data=data[i], index=None, columns=None) for i in range(len(lables)) ]
 # print(data_df[0][0])
 # data = data.reshape(42000,37,4)
-joint_num = 1
+joint_num = 0
 marker = data[joint_num]
 tmp = pd.DataFrame(data=marker.T[0], index=None, columns=None)
 # print(tmp.head())
@@ -47,25 +49,29 @@ tmp = pd.DataFrame(data=marker.T[0], index=None, columns=None)
 # print(data_df_list[0].head())
 
 [x,y,z] = data[0][0:3]
+
 # v_x = np.diff(x)
 # print(x.shape)
 
-derive_by_t = lambda vec: num_of_frames * np.diff(x) 
-velocities = [v_x, v_y, v_z] = list(map(derive_by_t, (x,y,z)))
+derive_by_t = lambda vec: num_of_frames * np.diff(vec) 
+velocities = [v_x, v_y, v_z] = list(map(derive_by_t, [x,y,z]))
 accelerations = [a_x, a_y, a_z] = list(map(derive_by_t, (v_x, v_y, v_z)))
 
 v_fft_decomp = list(map(abs, map(fft, velocities)))
 a_fft_decomp = list(map(abs,map(fft, accelerations)))
 
+print(len(v_fft_decomp[0]))
+print(len(v_fft_decomp[1]))
+print(len(v_x))
 dim = 3
 plot_linewidth = 0.5
 fig, axs = plt.subplots(4, dim, sharex=True)
 fig.suptitle('{}'.format(lables[joint_num]))
 for i in range(dim): 
     axs[0,i].plot(velocities[i], linewidth=plot_linewidth)
-    axs[1,i].plot(v_fft_decomp[i], c='green', linewidth=plot_linewidth)
+    axs[1,i].plot(v_fft_decomp[i][500:-500], c='green', linewidth=plot_linewidth)
     axs[2,i].plot(accelerations[i], c='orange', linewidth=plot_linewidth)
-    axs[3,i].plot(a_fft_decomp[i], c='red', linewidth=plot_linewidth)
+    axs[3,i].plot(a_fft_decomp[i][500:-500], c='red', linewidth=plot_linewidth)
 
     axs[0,i].set_title(['X component','Y component','Z component'][i])
     
@@ -80,3 +86,4 @@ plt.show()
 # ax.scatter(x, y, z, s=0.1)
 
 # plt.show()
+
--- pca_mat.py
+++ pca_mat.py
@@ -4,4 +4,25 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 
-print('hi')
+mat = sio.loadmat('data_files/AIM_model_movements0004.mat')
+num_of_frames = mat['AIM_model_movements0004']['Frames'][0][0].flatten()[0]
+delta_t_sec = 1 / num_of_frames
+frame_rate = mat['AIM_model_movements0004']['FrameRate'][0][0].flatten()[0]
+data = mat['AIM_model_movements0004']['Trajectories'][0][0]['Labeled'][0][0]
+
+lables = data['Labels'][0][0][0].flatten()
+
+lables_df = pd.DataFrame(data=lables, index=None, columns=None)
+data_count = data['Count'][0][0].flatten()[0]
+
+# data.shape -> (37, 4, 42000)
+data = data['Data'][0][0]
+data_df_list = [ pd.DataFrame(data=data[i], index=None, columns=None) for i in range(len(lables)) ]
+
+print(lables.shape)
+print(data[0].flatten().shape)
+
+samples_for_pca = dict()
+for i in range(len(lables)):
+    samples_for_pca[lables[i][0]] = data[i].flatten()
+
