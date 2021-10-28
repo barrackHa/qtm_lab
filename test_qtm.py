f0ec5307deb9997bf01e3ff3f8f12f4be613a708
Barrack Ha <barakiey@gmail.com>
2021-10-28 18:55:32 +0300

    add unittests

---
+++ test_qtm.py
@@ -0,0 +1,18 @@
+import pytest
+from fft_mat import QTM
+
+def test_test():
+    assert 1 == 1
+
+def test_fails():
+    with pytest.raises(AssertionError):
+        assert 1 == 2
+
+@pytest.fixture
+def mat_4_by_4_file():
+    return QTM('f')
+
+def test_4by4_shape(mat_4_by_4_file):
+    print(mat_4_by_4_file.data.shape)
+    assert mat_4_by_4_file.data.shape == (2,4,10)
+    
